f95b6ec0584d52e36bbc88749c7bd1dfe1444f56
Jeff Lapp <lappjeff20@gmail.com>
2022-10-25 20:16:59 -0400

    refactor popup to use class

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,75 +1,101 @@
-import { MessageType, VideoPayload } from "../models/message.model";
+import { byId, createElement, on } from "../libs/htmlUtils";
+import { getActiveTab, sendMessage } from "../libs/tabUtils";
+import { Input } from "../models/dom.model";
+import { VideoPayload } from "../models/message.model";
 import { VideoNote } from "../models/storage.model";
-import { YTParam } from "../models/youtube.model";
 
-const noteButton = document.getElementsByClassName("noteBtn")[0];
-const titleInput = document.getElementsByClassName(
-  "title"
-)[0] as HTMLInputElement;
-const descriptionInput = document.getElementsByClassName(
-  "description"
-)[0] as HTMLInputElement;
+class Popup {
+  private videoData: VideoNote;
+  private storage = browser.storage.sync;
 
-noteButton.addEventListener("click", async () => {
-  try {
-    await saveNote({
-      ...videoData,
-      title: titleInput.value,
-      description: descriptionInput.value,
+  public constructor() {
+    this.initPopup();
+    this.getVideoData();
+  }
+
+  private initPopup(): void {
+    const container = byId("popup-content");
+
+    const titleInput = createElement<HTMLInputElement>(
+      document,
+      container,
+      "input",
+      { name: Input.TITLE, placeholder: "Note name" }
+    );
+
+    const descriptionInput = createElement<HTMLTextAreaElement>(
+      document,
+      container,
+      "textarea",
+      { name: Input.DESCRIPTION, placeholder: "Note description" }
+    );
+
+    const inputs = [titleInput, descriptionInput];
+
+    for (let input of inputs) {
+      this.handleInput(input);
+    }
+
+    const saveBtn = createElement(document, container, "button", {
+      textContent: "Save",
     });
 
-    window.close();
-  } catch (error) {
-    console.error(error);
+    on(saveBtn, "click", async () => {
+      try {
+        await this.saveNote(this.videoData);
+
+        console.log(this.storage.get());
+        window.close();
+      } catch (error) {
+        console.error(error);
+      }
+    });
   }
-});
 
-const storage = browser.storage.sync;
+  private async getVideoData(): Promise<void> {
+    const activeTab = await getActiveTab();
 
-let videoData: VideoNote | null = null;
+    const {
+      timestamp: startTime,
+      videoId: id,
+      url,
+    } = await this.requestVideoData(activeTab.id);
 
-(async () => {
-  const activeTab = await getActiveTab();
+    this.videoData = {
+      startTime,
+      id,
+      url,
+      title: "",
+      description: "",
+    };
+  }
 
-  const { timestamp, videoId } = await requestVideoData(activeTab);
+  private handleInput(el: HTMLInputElement | HTMLTextAreaElement) {
+    on(el, "input", (e: Event) => {
+      const target = e.target as HTMLInputElement | HTMLTextAreaElement;
 
-  const timestampedUrl = `https://youtu.be/${videoId}?${YTParam.TIMESTAMP}=${timestamp}`;
+      this.videoData = { ...this.videoData, [target.name]: target.value };
+    });
+  }
 
-  videoData = {
-    startTime: timestamp,
-    id: videoId,
-    url: timestampedUrl,
-    title: "",
-    description: "",
-  };
-})();
+  async requestVideoData(tabId: number): Promise<VideoPayload> {
+    const videoData = sendMessage<VideoPayload>(
+      "request_video_metadata",
+      tabId
+    );
 
-async function saveNote(video: VideoNote): Promise<void> {
-  const existingNotes: { [videoId: string]: VideoNote[] } = await storage.get(
-    video.id
-  );
+    return videoData;
+  }
 
-  await storage.set({
-    [video.id]: [video, ...(existingNotes[video.id] || [])],
-  });
+  async saveNote(video: VideoNote): Promise<void> {
+    const existingNotes: { [videoId: string]: VideoNote[] } =
+      await this.storage.get(video.id);
+
+    await this.storage.set({
+      [video.id]: [video, ...(existingNotes[video.id] || [])],
+    });
+  }
 }
 
-async function getActiveTab(): Promise<browser.tabs.Tab> {
-  const tab = (
-    await browser.tabs.query({
-      currentWindow: true,
-      active: true,
-    })
-  )[0] as browser.tabs.Tab;
-
-  return tab;
-}
-
-async function requestVideoData(tab: browser.tabs.Tab): Promise<VideoPayload> {
-  const videoData: VideoPayload = await browser.tabs.sendMessage(
-    tab.id,
-    MessageType.REQUEST_VIDEO_METADATA
-  );
-
-  return videoData;
-}
+// init popup
+new Popup();
