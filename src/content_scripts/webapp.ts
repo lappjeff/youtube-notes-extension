0e922f1ad13bbcdf37b762aa4aada74cd3d035cd
Jeff Lapp <lappjeff20@gmail.com>
2022-10-27 10:31:44 -0400

    wip: save items to local storage!

--- src/content_scripts/webapp.ts
+++ src/content_scripts/webapp.ts
@@ -1,8 +1,9 @@
 import { Message, MessageType } from "../models/message.model";
-import browser from "webextension-polyfill";
 
 browser.runtime.onMessage.addListener(async (message: Message) => {
   if (message.type === MessageType.SYNC_DATA) {
-    await browser.storage.local.set({ "yt-notes": message.payload });
+    window.localStorage.setItem("yt-notes", JSON.stringify(message.payload));
+
+    window.dispatchEvent(new Event("storage"));
   }
 });
